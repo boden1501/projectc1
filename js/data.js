d7de0553065e705aa37343b08c85544fc1deb5a2
boden1501 <htvinh1501@gmail.com>
2023-05-12 19:21:35 +0700

    first commit

---
+++ js/data.js
@@ -0,0 +1,11 @@
+const category=[
+    {cat_id:'phone',cat_name:'Điện thoại' }, 
+    {cat_id:'latop',cat_name:'Máy tính' },
+    {cat_id:'ram',cat_name:'Bộ nhớ trong' }
+]
+const product=[
+    {id:1,name:`Samsung Galaxy S23 Ultra 256GB`,price:'20.450.000 ₫',img:'https://cdn2.cellphones.com.vn/358x358,webp,q100/media/catalog/product/s/2/s23-ultra-tim.png',cat_id:'phone'},
+    {id:2,name:`iPhone 14 Pro Max 128GB`,price:'26.450.000 ₫',img:'https://cdn2.cellphones.com.vn/358x358,webp,q100/media/catalog/product/t/_/t_m_18.png',cat_id:'phone'},
+    {id:3,name:`Laptop Dell Inspiron 3511 5829BLK`,price:'13.490.000 ₫',img:'https://cdn2.cellphones.com.vn/358x358,webp,q100/media/catalog/product/t/e/text_ng_n_3__1_71.png',cat_id:'laptop'}
+
+]
